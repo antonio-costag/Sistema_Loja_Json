adda9a8771418d7612f9d01e07e30789517a9637
antonio_costag <antonio.neto.cost@gmail.com>
2026-02-09 15:46:54 -0300

    consertanto o nome main

---
+++ codigos/main.py
@@ -0,0 +1,121 @@
+from produtos import Produto
+from gerente_banco import GerenteBanco
+from rich import print
+from rich.table import Table
+from rich.panel import Panel
+from rich.text import Text
+import os
+
+gb = GerenteBanco()
+
+def TelaInicial():
+    os.system('clear')
+
+    tabela_desig = Table(title="[yellow]Tabela de Inicial[/]", style="yellow")
+
+    tabela_desig.add_column("[yellow]Opções[/]", style="blue", width=30)
+
+    tabela_desig.add_row("Cadastrar Produto")
+    tabela_desig.add_row("Buscar Produto")
+    tabela_desig.add_row("Sair")
+
+    print(tabela_desig)
+
+    opcao = input("Escolha uma opção: ")
+
+    match opcao:
+        case "Cadastrar Produto":
+            TabelaCadastroProduto()
+        case "Buscar Produto":
+            TelaBuscaProduto()
+        case "Sair":
+            os.system('clear')
+
+            texto = Text.from_markup("bya:sweat:", justify='center')
+            panel = Panel(texto, title="Menssagem", style="blue", width=34)
+            print(panel)
+
+            return
+        case _:
+            texto = Text.from_markup("Opção invalida:sweat:", justify='center')
+            panel = Panel(texto, title="Menssagem", style="red", width=34)
+            print(panel)
+
+            input("\nPressione Enter para continuar...")
+            TelaInicial()
+    pass
+
+def TabelaCadastroProduto():
+    os.system('clear')
+
+    texto = Text.from_markup("Cadastro de Porduto:+1:", justify='center')
+    panel = Panel(texto, title="Menssagem", style="yellow", width=34)
+    print(panel)
+
+    nome = input("Digite o nome do produto: ")
+    valor = float(input("Digite o valor do produto: "))
+    categoria = input("Digite a categoria do produto: ")
+    estoque = int(input("Digite o estoque do produto: "))
+
+    produto1 = Produto(nome, valor, categoria, estoque)
+    gb.produto = produto1
+    gb.CadastrarProduto()
+
+    input("\nPressione Enter para continuar...")
+    TelaInicial()
+
+    pass
+
+def TelaBuscaProduto():
+    os.system('clear')
+
+    #isso é só pra deixar mais bonito
+    tabela_desig = Table(title="[yellow]Tabela de Busca[/]", style="yellow")
+
+    tabela_desig.add_column("[yellow]Opções[/]", style="blue", width=30)
+
+    tabela_desig.add_row("id_produto")
+    tabela_desig.add_row("nome")
+    tabela_desig.add_row("valor")
+    tabela_desig.add_row("categoria")
+    tabela_desig.add_row("estoque")
+    tabela_desig.add_row("todos")
+
+    print(tabela_desig)
+
+    tabela = input("Escolha uma opção: ")
+
+    list = ["id_produto", "nome", "valor", "categoria", "estoque", "todos"]
+
+    #aqui que eu fiz esse ciclo de repetição pra ser mais rapido eu verificar se a opção existe
+    achou_valor = False
+    for i in list:
+        if i == tabela:
+            achou_valor = True
+            break
+
+    #se a opção não existe eu printo uma mensagem de erro e busco novamente 
+    if not achou_valor:
+        texto = Text.from_markup("Opção invalida:sweat:", justify='center')
+        panel = Panel(texto, title="Menssagem", style="red", width=34)
+        print(panel)
+
+        input("\nPressione Enter para continuar...")
+
+        TelaBuscaProduto()
+    else:
+
+        #caso que queira buscar todos os valores, o nome da busca se torna inutil
+        nome_busca = 0
+        if tabela != "todos":
+            nome_busca = input(f"digite o(a) {tabela} do seu profuto: ")
+
+        gb.BuscarProduto(tabela, nome_busca)
+
+        input("\nPressione Enter para continuar...")
+        TelaInicial()
+
+    pass
+
+
+TelaInicial()
