f7c29038b2f1f510157c503ecc99e52527ee30b0
antonio_costag <antonio.neto.cost@gmail.com>
2026-02-13 10:37:04 -0300

    carrinho, finalizado com sucesso :)

--- codigos/carrinho.py
+++ codigos/carrinho.py
@@ -21,8 +21,6 @@ class Carrinho:
         Docstring para AdicionarCarrinho
         
         Função para adicionar os produtos no carrinho
-
-        (ainda tem alguns problemas de logica com meu banco)
         """
         os.system('clear')
 
@@ -31,20 +29,42 @@ class Carrinho:
         id = int(input("Digite o valor do id para adicionar no carrinho: "))
         quantidade = int(input("Quantos produtos deseja adicionar no carrinho: "))
 
+        
+        # proibindo a entrada de valores negativos ou zero, pois não faria sentido remover um produto com quantidade negativa ou zero
+        if id <= 0 or quantidade <= 0:
+            texto = Text.from_markup("ID e quantidade devem ser maiores que zero:sweat:", justify='center')
+            panel = Panel(texto, title="Erro", style="red", width=34)
+            print(panel)
+            input("\nPressione Enter para continuar...")
+            self.AdicionarCarrinho()
+            return
+        
+
         with open(caminho_banco, 'r', encoding='utf-8') as arquivo:
             dados = json.load(arquivo)
 
-        achou_o_id = False
+        achou_o_id_no_banco = False
         #precorendo todos os ids do meu banco
         for produto in dados['produtos']:
             if id == produto['id_produto']:
-                achou_o_id = True
+                achou_o_id_no_banco  = True
 
-                #se eu achar o id e ele estiver em estoque no banco, eu removo e adiciono no carrinho
-                #isso é um problema, pois se houver algum probelama antes de eu finalizar a compra
-                #o produto vai sumir do meu banco, gerando prejuizos
+                #uma verificação pra saber se meu produto ja esta no carrinho
+                achou_o_id_no_carrinho = False
+                #uso essa estrutura de repetição pra conseguir saberem qual idice meu produto esta
+                for indice, id_produto in enumerate(self.id_produto):
+                    if id == id_produto:
+                        indice_produto = indice
+                        achou_o_id_no_carrinho = True
+
+                #se a quantidade que eu quero do produto estiver em estoque
                 if produto['estoque'] >= quantidade and produto['estoque'] > 0:
-                    self.quantidade.append(quantidade)
+                    #eu verifico se tenho que adicionar essa quantidade a um novo produto
+                    if not achou_o_id_no_carrinho:
+                        self.quantidade.append(quantidade)
+                        #ou apenas somar em um produt ja existente
+                    else:
+                        self.quantidade[indice_produto] += quantidade
                 else:
 
                     #mensagem de erro caso o produto não estaja em estoque
@@ -53,27 +73,23 @@ class Carrinho:
                     print(panel)
 
                     input("\nPressione Enter para continuar...")
-
                     return
                 
-                #adicionando os valores que eu preciso
-                self.id_produto.append(id)
-                self.valor += produto['valor']
+                #aqui eu verifico se meu produto ja esta no carrinho, caso não esteja, eu adiciono
+                if not achou_o_id_no_carrinho:
+                    self.id_produto.append(id)
+                self.valor += (produto['valor'] * quantidade)
 
         #mensagem de erro caso eu não ache o id no banco
-        if not achou_o_id:
-            texto = Text.from_markup("Produto fora de estoque:sweat:", justify='center')
-            panel = Panel(texto, title="Menssagem", style="red", width=34)
+        if not achou_o_id_no_banco:
+            texto = Text.from_markup("ID inválido:sweat:", justify='center')
+            panel = Panel(texto, title="Erro", style="red", width=34)
             print(panel)
 
             input("\nPressione Enter para continuar...")
 
             self.AdicionarCarrinho()
-
-        #caso tudo dê certo, eu atualizo o banco
-        with open(caminho_banco, 'w', encoding='utf-8') as arquivo:
-            json.dump(dados, arquivo, indent=4, ensure_ascii=False)
-
+            return
         BP.BuscarBanco("todos", 0)
         pass
 
@@ -97,49 +113,88 @@ class Carrinho:
         pass
 
     def RemoverCarrinho(self):
+        """
+        Docstring para RemoverCarrinho
+        
+        função criada com o intuito de acessar o carrinho e remover valores dele
+        """
         os.system('clear')
 
         self.ExibirCarrinho()
 
         id = int(input("Digite o valor do id para remover do carrinho: "))
+        quantidade = int(input("Quantos produtos deseja remover do carrinho: "))
 
-        achou_o_id = False
-        for i in self.id_produto:
-            if id == i:
-                achou_o_id = True
+        
+        # proibindo a entrada de valores negativos ou zero, pois não faria sentido remover um produto com quantidade negativa ou zero
+        if id <= 0 or quantidade <= 0:
+            texto = Text.from_markup("ID e quantidade devem ser maiores que zero:sweat:", justify='center')
+            panel = Panel(texto, title="Erro", style="red", width=34)
+            print(panel)
+            input("\nPressione Enter para continuar...")
+            self.RemoverCarrinho()
+            return
+        
 
-        if not achou_o_id:
-            texto = Text.from_markup("Produto fora de estoque:sweat:", justify='center')
-            panel = Panel(texto, title="Menssagem", style="red", width=34)
+        #percorrendo o carrinho pra saber se o item que eu quero remover existe no carrinho 
+        achou_o_id_no_carrinho = False
+        for indice, id_produto in enumerate(self.id_produto):
+            if id == id_produto:
+                indice_produto = indice
+                achou_o_id_no_carrinho = True
+
+        #caso o item não exista, exibo uma menssagem de erro, e retomo a operação
+        if not achou_o_id_no_carrinho:
+            texto = Text.from_markup("Produto fora do carrinho:sweat:", justify='center')
+            panel = Panel(texto, title="Erro", style="red", width=34)
             print(panel)
 
             input("\nPressione Enter para continuar...")
 
             self.RemoverCarrinho()
+            return
 
 
         with open(caminho_banco, 'r', encoding='utf-8') as arquivo:
             dados = json.load(arquivo)
 
+        #percorrendo meu banco de produtos
         for produto in dados['produtos']:
+            #procurando os item que estão no meu carrinho no banco
             if id == produto['id_produto']:
-                produto['estoque'] += 1
 
-                self.id_produto.remove(id)
-                self.valor -= produto['valor']
+                #verifica se eu tenho essa quantidade que eu quero remover do carrinho
+                if quantidade <= self.quantidade[indice_produto]:
 
-        with open(caminho_banco, 'w', encoding='utf-8') as arquivo:
-            json.dump(dados, arquivo, indent=4, ensure_ascii=False)
+                    #se tiver eu removo do carrinho e do valor
+                    self.quantidade[indice_produto] -= quantidade
+                    self.valor -= (produto['valor'] * quantidade)
+
+                    #se não sobrar nenhum produto do tipo no meu carrinho, eu removo ele
+                    if self.quantidade[indice_produto] == 0: 
+                        self.id_produto.pop(indice_produto)
+                        self.quantidade.pop(indice_produto)
+                else:
+                    texto = Text.from_markup("Quantidade inválida:sweat:", justify='center')
+                    panel = Panel(texto, title="Erro", style="red", width=34)
+                    print(panel)
+
+                    input("\nPressione Enter para continuar...")
+
+                    self.RemoverCarrinho()
 
         BP.BuscarBanco("todos", 0)
         pass
 
 c = Carrinho()
 
-c.AdicionarCarrinho()
-for i in range((len(c.id_produto))):
-    print(f"{c.id_produto[i]}, {c.quantidade[i]}")
-    
+while True:
+    c.AdicionarCarrinho()
 
-c.RemoverCarrinho()
-#print(c.valor)
+    c.RemoverCarrinho()
+    print("\n")
+
+    c.ExibirCarrinho()
+    print(f"R${c.valor:,.2f}")
+
+    input("\nPressione Enter para continuar...")
